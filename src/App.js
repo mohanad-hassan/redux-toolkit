55b9ba85b97e88c55b866212186f1a032ccda258
mohanad hassan <mohanadhassan969696@gmail.com>
2023-04-14 23:59:31 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { calcuateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
-
+import { getCartItems } from "./features/cart/cartSlice";
 import Modal from "./compnents/Modal";
 
 
 function App() {
   const dispatch = useDispatch()
-const {cartItems} = useSelector((store) => store.cart)
+const {cartItems,isLoading} = useSelector((store) => store.cart)
 const { isOpen } = useSelector((store) => store.modal);
 //this useEffect riggers the component to rernder with every change ofd cartItenms so this is bad aproach .
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
   useEffect(() => {
     dispatch(calcuateTotals());
   }, [cartItems]);
 
+
+    if (isLoading) {
+      return (
+        <div className="loading">
+          <h1>Loading...</h1>
+        </div>
+      );
+    }
   return (
     <main>
 {     isOpen&& <Modal />
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
+import axios from "axios";
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 1,
   total: 0,
   isLoading: true,
 };
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState()); returns the whole store of redux .
+      // thunkAPI.dispatch(openModal());   you can dispatch any thing .
+      const resp = await axios(url);
+
+      return resp.data;
+    } catch (error) {
+      //it will pass the error to action.payload 
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -32,18 +53,29 @@ const cartSlice = createSlice({
       });
       cartItem.amount = cartItem.amount - 1;
     },
-    calcuateTotals :(state) => { 
-      let total=0 ;
-      let amount=0 ;
-state.cartItems.forEach((item) => { 
-
-  amount+= item.amount
-  total+= item.amount * item.price
-
- })
-state.total=total
-state.amount= amount
-     }
+    calcuateTotals: (state) => {
+      let total = 0;
+      let amount = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.total = total;
+      state.amount = amount;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
   },
 });
 
